42ae7dabfe315a5506353085aee95bd08fdd9357
AZH2002 <73726592+AZH2002@users.noreply.github.com>
2021-12-11 19:39:45 -0500

    Add files via upload

---
+++ JulianSQL/empleado_proyecto.sql
@@ -0,0 +1,16 @@
+	INSERT INTO Empleado_Proyecto(	"	,	);		INSERT INTO Empleado_Proyecto("123456789","1","32.5","2015-1012");						
+	INSERT INTO Empleado_Proyecto(	"	,	);		INSERT INTO Empleado_Proyecto("123456789","2","7.5","2015-1012");						
+	INSERT INTO Empleado_Proyecto(	"	,	);		INSERT INTO Empleado_Proyecto("666884444","3","40.0","2015-1001");						
+	INSERT INTO Empleado_Proyecto(	"	,	);		INSERT INTO Empleado_Proyecto("453453453","1","20.0","2015-1002");						
+	INSERT INTO Empleado_Proyecto(	"	,	);		INSERT INTO Empleado_Proyecto("453453453","2","20.0","2015-0409");						
+	INSERT INTO Empleado_Proyecto(	"	,	);		INSERT INTO Empleado_Proyecto("333445555","2","10.0","2015-0410");						
+	INSERT INTO Empleado_Proyecto(	"	,	);		INSERT INTO Empleado_Proyecto("333445555","3","10.0","2015-1012");						
+	INSERT INTO Empleado_Proyecto(	"	,	);		INSERT INTO Empleado_Proyecto("333445555","10","10.0","1988-0412");						
+	INSERT INTO Empleado_Proyecto(	"	,	);		INSERT INTO Empleado_Proyecto("333445555","20","10.0","2015-1012");						
+	INSERT INTO Empleado_Proyecto(	"	,	);		INSERT INTO Empleado_Proyecto("999887777","30","30.0","2015-1012");						
+	INSERT INTO Empleado_Proyecto(	"	,	);		INSERT INTO Empleado_Proyecto("999887777","10","10.0","2015-1013");						
+	INSERT INTO Empleado_Proyecto(	"	,	);		INSERT INTO Empleado_Proyecto("987987987","10","35.0","2015-1016");						
+	INSERT INTO Empleado_Proyecto(	"	,	);		INSERT INTO Empleado_Proyecto("987987987","30","5.0","2015-0417");						
+	INSERT INTO Empleado_Proyecto(	"	,	);		INSERT INTO Empleado_Proyecto("987654321","30","20.0","2015-0418");						
+	INSERT INTO Empleado_Proyecto(	"	,	);		INSERT INTO Empleado_Proyecto("987654321","20","15.0","2015-0419");						
+	INSERT INTO Empleado_Proyecto(	"	,	);		INSERT INTO Empleado_Proyecto("888775555","20","12.6","2015-0420");						
